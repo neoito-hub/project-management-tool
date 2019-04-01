46c5fbe28165d3b8c77c6806d268775e6ad3b6f2
Abin Punnoose <abin.punnoose@neoito.com>
2019-04-01 20:46:23 +0530

    updated assets folder

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  private avatar = require('../shared/images/img_avatar2.png');
+  private avatar = 'assets/images/img_avatar2.png';
 
   constructor() {}
 
