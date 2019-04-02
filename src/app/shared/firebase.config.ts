5ecb5ac1e58c896169fe75fb41810006448869ec
Abin Punnoose <abin.punnoose@neoito.com>
2019-04-02 11:26:48 +0530

    removed firebase config

--- src/app/shared/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCxnHFER2w3CDJ_qUA39XYUP2AmgG9TRHQ',
-  authDomain: 'project-management-tool-2ce49.firebaseapp.com',
-  databaseURL: 'https://project-management-tool-2ce49.firebaseio.com',
-  projectId: 'project-management-tool-2ce49',
-  storageBucket: 'project-management-tool-2ce49.appspot.com',
-  messagingSenderId: '765445839148'
-};
