cad9d69bafbdf319f6ea540ff8adf522ed64176b
luks <lukhman.latheef@gmail.com>
2019-04-25 14:34:44 +0530

    minor optimisation

--- src/app/modules/project/container/list-project/list-project.component.ts
+++ src/app/modules/project/container/list-project/list-project.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import * as Project from '../../../../core/project';
 import { Observable } from 'rxjs';
+import { projectStatusList } from '../../../../shared/const-values';
 
 @Component({
   selector: 'app-project-container',
@@ -12,7 +13,7 @@ import { Observable } from 'rxjs';
 export class ListProjectContainerComponent implements OnInit {
   public projects$: Observable<any>;
 
-  status = ['In Progress', 'On Hold', 'Completed', 'Closed'];
+  status = projectStatusList.map(item => item.status);
   constructor(public _store: Store<any>, private router: Router) {
     this._store.dispatch(new Project.LoadProjectAction());
     this.projects$ = this._store.select(Project.getAllProjects);
